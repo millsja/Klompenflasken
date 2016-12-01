149f9ee8f5a847ef173f5c9cb86743e614936c17
Nate Piche <npiche@zipcar.com>
2016-12-01 13:01:07 +0000

    final update for email service

--- email_service/app.rb
+++ email_service/app.rb
@@ -11,7 +11,7 @@ require './services/latex_service'
 require './services/email_service'
 
 get "/health" do
-  "Latex service really healthy"
+  'Latex service really healthy'
 end
 
 get "/logs/errors" do
@@ -33,6 +33,9 @@ end
 
 post "/generate" do
   post_params = JSON.parse(request.body.read)
+  post_params_for_logs = post_params.clone
+  post_params_for_logs['awarder_signature'] = 'Hidden base64 encoding'
+
   log_params = {
     shared_log_id: SecureRandom.uuid,
     method: request.request_method,
@@ -40,10 +43,10 @@ post "/generate" do
     received: Time.now
   }
 
-  RequestLog.new(log_params.merge(info: post_params)).save
+  RequestLog.new(log_params.merge(info: post_params_for_logs)).save
   maybe_sent_before = SentCertificate.sent_before?(
-    email: post_params["recipient_email"],
-    award: post_params["award_type"]
+    email: post_params['recipient_email'],
+    award: post_params['award_type']
   )
 
   if maybe_sent_before.size == 1
@@ -61,8 +64,8 @@ post "/generate" do
 
     email_response = email.send
 
-    unless email_response.status_code == "202"
-      email_errors = JSON.parse(email_response.body)["errors"].first
+    unless email_response.status_code == '202'
+      email_errors = JSON.parse(email_response.body)['errors'].first
       ErrorLog.new(log_params.merge(info: email_errors)).save
 
       content_type :json
@@ -80,15 +83,15 @@ post "/generate" do
     latex = LatexService.new(post_params)
     pdf_path = latex.generate_pdf
     email = EmailService.new(
-      to: post_params["recipient_email"],
-      award: post_params["award_type"]
+      to: post_params['recipient_email'],
+      award: post_params['award_type']
     )
     email.build_attachment(pdf_path)
 
     email_response = email.send
 
-    unless email_response.status_code == "202"
-      email_errors = JSON.parse(email_response.body)["errors"].first
+    unless email_response.status_code == '202'
+      email_errors = JSON.parse(email_response.body)['errors'].first
       ErrorLog.new(log_params.merge(info: email_errors)).save
 
       content_type :json
@@ -96,8 +99,8 @@ post "/generate" do
     end
 
     SentCertificate.new(
-      to_email: post_params['email'],
-      award_name: post_params['award'],
+      to_email: post_params['recipient_email'],
+      award_name: post_params['award_type'],
       encoded_attachment: email.attachment.content
     ).save
   end
